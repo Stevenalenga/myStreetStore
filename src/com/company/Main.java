b1c01a526a99bf96ef290558eeab5771b2f1e5d4
steven.alenga <steven.alenga@gmail.com>
2021-02-06 13:13:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public void myStreet() {
+
+        }
+        public void myProducts(){
+
+        }
+        public void myPaycode(){
+
+        }
+        public void aboutUs(){
+
+        }
+    }
+}
